a27e3d4c44e52f6e7eef5aecdc71f6adfdadf3ce
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-28 00:09:45 +0300

    2(إنشاء قاعدة البيانات + عمل فولدر للرواوت الخاص باللوحة+اعدادات للكونترولر )

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -15,6 +15,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     protected $namespace = 'App\Http\Controllers';
+    protected $dashboard_namespace = 'App\Http\Controllers\Dashboard';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -39,6 +40,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapDashboardWebRoutes();
+
         //
     }
 
@@ -54,6 +57,13 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
+    }
+
+    protected function mapDashboardWebRoutes()
+    {
+        Route::middleware('web')
+            ->namespace($this->dashboard_namespace)
+            ->group(base_path('routes/dashboard/web.php'));
     }
 
     /**
