42f53926956cc7ae77c9247c55c6be1ebacbf2fb
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-28 00:14:48 +0300

    2.2(عمل test على الإعدادات الجديدة للكونترولر وبالأخص للراوت )

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -1,2 +1,9 @@
 <?php
 
+Route::prefix('dashboard')->name('dashboard.')->group(function (){
+
+    Route::get('/check',function (){
+        return "this is Dashboard";
+    });
+
+});//end of dashboard routes
