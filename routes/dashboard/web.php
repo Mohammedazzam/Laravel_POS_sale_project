5ec0e42d28ac0034aafd6476fb50e892993265d8
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-28 19:50:17 +0300

    3.2(تجهيز كامل اللوحة مع تقسيمة الملفات للمشروع بشكل كامل)

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -3,7 +3,7 @@
 Route::prefix('dashboard')->name('dashboard.')->group(function (){
 
     Route::get('/check',function (){
-        return "this is Dashboard";
+        return view('dashboard.index');
     });
 
 });//end of dashboard routes
