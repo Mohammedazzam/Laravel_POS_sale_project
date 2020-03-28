224a522759487745b77640a32627350a6dee2a38
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-29 02:09:21 +0300

    3.4(عمل جزئية اللغة )

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -2,6 +2,6 @@
 
 Route::prefix('dashboard')->name('dashboard.')->group(function() {
 
-    Route::get('/index', 'DashboardController@index')->name('dashboard.index');
+    Route::get('/index', 'DashboardController@index')->name('index');
 
 });//end of dashboard routes
