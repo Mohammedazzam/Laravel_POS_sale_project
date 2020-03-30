043e8f6093ba4b70c1528bc766411a618be408df
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-03-30 16:22:51 +0300

    8.3(البدء في عمل صفة ال create لليوزر)

--- resources/views/dashboard/users/create.blade.php
+++ resources/views/dashboard/users/create.blade.php
@@ -6,18 +6,30 @@
 
         <section class="content-header">
 
-            <h1>Blank page
-                <small>it all starts here</small>
-            </h1>
+                <h1>@lang('site.users')</h1>
 
-            <ol class="breadcrumb">
-                <li class="active"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</li>
-            </ol>
+                <ol class="breadcrumb">
+                    <li><a href="{{route('dashboard.index')}}"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</a></li>
+                    <li><a href="{{route('dashboard.users.index')}}">@lang('site.users')</a></li>
+                    <li class="active">@lang('site.add')</li>
+                </ol>
         </section>
 
         <section class="content">
 
+            <div class="box box-primary">
 
+                <div class="box-header">
+
+                    <h3 class="box-title">@lang('site.add')</h3>
+
+                </div><!--end of box header-->
+
+                <div class="box-body">
+
+                </div>
+
+            </div><!--end of box-->
 
         </section><!-- end of content -->
 
